782fec8cf65d3f2ab0df00275b9e6cb8d9265d5b
EdmilsonAlison <edmilson.alisondias@gmail.com>
2022-07-19 15:22:49 -0300

    First Commit

---
+++ src/main/java/com/eddiediasdev/restwithjavaspringboot/RestWithJavaSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.eddiediasdev.restwithjavaspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestWithJavaSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestWithJavaSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eddiediasdev/restwithjavaspringboot/RestWithJavaSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eddiediasdev.restwithjavaspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestWithJavaSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
